23bceebbbaec4921b8099972ba68844bcc44a364
rosmarenriquez <rosmar.em@outlook.es>
2024-06-01 21:34:06 -0600

    changes made

--- javascript/site.js
+++ javascript/site.js
@@ -12,6 +12,15 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+const currentYearInfo = new Date().getFullYear();
+const lastModifiedInfo = document.lastModified;
+
+const currentYear = document.getElementById("currentYear");
+const lastModified = document.getElementById("lastModified");
+
+currentYear.innerHTML = currentYearInfo;
+lastModified.innerHTML = `Last Modification: ${lastModifiedInfo}`;
+
 let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
