a21477494f168e2231070573cea28e1deb724752
rosmarenriquez <rosmar.em@outlook.es>
2024-06-01 20:04:08 -0600

    adding some CSS

--- javascript/site.js
+++ javascript/site.js
@@ -12,38 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Form validation
-    const contactForm = document.getElementById('contactForm');
-    contactForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
-        
-        if (validateEmail(email) && validateMessage(message)) {
-            saveToLocalStorage(name, email, message);
-            alert('Form submitted successfully!');
-            contactForm.reset();
-        } else {
-            alert('Please enter valid email and message.');
-        }
-    });
-
-    function validateEmail(email) {
-        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return re.test(email);
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
     }
+  }
 
-    function validateMessage(message) {
-        return message.trim().length > 0;
-    }
-
-    function saveToLocalStorage(name, email, message) {
-        const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
-        contacts.push({ name, email, message });
-        localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-});
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
