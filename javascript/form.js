3ac0c9014c7bebef64e5b13e1549a7cde76b3b63
rosmarenriquez <rosmar.em@outlook.es>
2024-06-01 23:28:10 -0600

    changes made

--- javascript/form.js
+++ javascript/form.js
@@ -31,3 +31,26 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem('contacts', JSON.stringify(contacts));
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('mouseover', () => {
+            link.style.color = '#ffeb3b';
+        });
+
+        link.addEventListener('mouseout', () => {
+            link.style.color = '#fff';
+        });
+    });
+});
+
+const currentYearInfo = new Date().getFullYear();
+const lastModifiedInfo = document.lastModified;
+
+const currentYear = document.getElementById("currentYear");
+const lastModified = document.getElementById("lastModified");
+
+currentYear.innerHTML = currentYearInfo;
+lastModified.innerHTML = `Last Modification: ${lastModifiedInfo}`;
